290549436e5769a9df17e5e47da66b443c810846
hirwa-nshuti <hirwanshutiflx@gmail.com>
2020-12-29 13:47:20 +0200

    Updated Palindrome for Vs code

--- Palindrome Number.py
+++ Palindrome Number.py
@@ -1,12 +1,17 @@
 #Python code to specify if a given integer is Palindrome
 
-def isPalindrome(self,x):
+def isPalindrome(x):
+    #converting our integer into a string
     x_string= str(x)
+    
+    #reversing the string obtained
     x_string_reversed=''.join(reversed(x_string))
 
+    #checking if the reversed string is the same as the original one
     if x_string == x_string_reversed:
         return True
     else:
         return False
 
-x=input("Enter the integer")
+integer=input("Enter the integer ")
+print(isPalindrome(integer))
