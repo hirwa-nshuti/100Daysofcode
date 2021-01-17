a647242c55ad2587292819d55e83d6ad2f7c4562
Felix <hirwanshutiflx@gmail.com>
2021-01-17 20:43:17 +0200

    Computing the Factorial of a given integer n

---
+++ Factorial.py
@@ -0,0 +1,14 @@
+class Factorial:
+    def factorial(self,n):
+        if n<0:
+            return 0
+        elif n==0 or n==1:
+            return 1
+        else:
+            fact=1
+            while(n):
+                fact=fact*n
+                n-=1
+            return fact
+
+print(Factorial().factorial(4))
