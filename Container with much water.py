9e65ce4c18496b0c38d81ef88d1acfd3103015f3
Felix <hirwanshutiflx@gmail.com>
2021-02-17 19:56:10 +0200

    Container with Much water

--- Container with much water.py
+++ Container with much water.py
@@ -1,3 +1,13 @@
+'''
+Given n non-negative integers a1, a2, ..., an , 
+where each represents a point at coordinate (i, ai). 
+n vertical lines are drawn such that the two endpoints 
+of the line i is at (i, ai) and (i, 0). Find two lines,
+ which, together with the x-axis forms a container, 
+ such that the container contains the most water.
+
+Notice that you may not slant the container.
+'''
 class Area:
     def maxArea(self, height):
             start, end = 0, len(height) - 1
