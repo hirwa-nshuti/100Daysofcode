fc6457315e152cef8ba281df52695ad15e0a77f6
Felix Hirwa <hirwanshutiflx@gmail.com>
2021-03-25 21:17:25 +0200

    Tree Implementation

---
+++ Tree Implementation.py
@@ -0,0 +1,8 @@
+"""
+Suppose you have a stock that sells Electronics
+and you have three different categories ach with its other types
+Phonrs: Iphone, Motorola, Google Pixel, Nokia
+TVs: Samung, LG, Skyworth
+Laptops: MacBook, Lenovo, Microsoft Surface
+
+This products Hierarchy can be presented using tree Data Structure
