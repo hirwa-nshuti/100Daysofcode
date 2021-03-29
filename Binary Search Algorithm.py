08c7fbbad5c7314e0f793d715ff872ea3a3793a7
Felix Hirwa <hirwanshutiflx@gmail.com>
2021-03-29 22:21:56 +0200

    Binary Search Algorithm

---
+++ Binary Search Algorithm.py
@@ -0,0 +1,32 @@
+def linear_search(num_list,num_to_find):
+    for index ,elment in enumerate(num_list):
+        if elment == num_to_find:
+            return index
+    return -1
+
+
+def binary_search(num_list,num_to_find):
+    left_index = 0
+    right_index = len(num_list)-1
+    mid_index = 0
+
+    while left_index < right_index:
+        mid_index = (left_index + right_index)//2
+        mid_number = num_list[mid_index]
+        if mid_number == num_to_find:
+            return mid_index
+        if mid_number < num_to_find:
+            left_index = mid_index + 1
+        else:
+            right_index = mid_index-1
+
+    return -1
+
+
+
+if __name__ == "__main__":
+    nums_list = [12,15,17,19,21,24,45,67]
+    num_find = 100
+
+    index = binary_search(nums_list,num_find)
+    print(f"Number of index is: {index} using Linear Search")
